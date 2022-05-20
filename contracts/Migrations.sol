beeb29579170f1b8273533c21d6dbfd15086fe42
deedatbilla <deedat5@yahoo.com>
2022-05-20 22:00:05 +0000

    initial commit

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.4.17;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.7.0;
 
 contract Migrations {
   address public owner;
@@ -8,16 +9,11 @@ contract Migrations {
     if (msg.sender == owner) _;
   }
 
-  function Migrations() public {
+  constructor() public {
     owner = msg.sender;
   }
 
   function setCompleted(uint completed) public restricted {
     last_completed_migration = completed;
-  }
-
-  function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
   }
 }
