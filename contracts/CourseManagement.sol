d729f0716512eed3409d63be2ee970eb7d43e952
deedatbilla <deedat5@yahoo.com>
2022-05-21 10:19:03 +0000

    configured react context api and added buy function to smart contract

--- contracts/CourseManagement.sol
+++ contracts/CourseManagement.sol
@@ -2,43 +2,42 @@
 pragma solidity >=0.4.21 <8.10.0;
 
 contract CourseManagement {
-  struct Course {
-    uint id;
-    string name;
-    uint price;
-    uint[] students; // all buyers of a course
-    string imgURL;
-  }
+    struct Course {
+        uint256 id;
+        string name;
+        address payable ownerAddress;
+        uint256 price;
+        uint256[] students; // all buyers of a course
+        string imgURL;
+    }
 
-  //buyer
-  struct Student {
-    uint id;
-    string name;
-    uint[] courses; //students owned courses
-  }
+    //buyer
+    struct Student {
+        uint256 id;
+        string name;
+        uint256[] courses; //students owned courses
+    }
 
-  //seller
-  struct Instructor {
-    uint id;
-    string name;
-    uint[] courses; //instructors uploaded courses
-  }
-  event courseAdded(string message);
-  mapping(uint => Course) courses;
-  mapping(address => Instructor) instructors;
-  mapping(address => Student) students;
+    //seller
+    struct Instructor {
+        uint256 id;
+        string name;
+        uint256[] courses; //instructors uploaded courses
+    }
+    event courseAdded(string message);
+    mapping(uint256 => Course) allCourses;
+    mapping(address => Instructor) instructors;
+    mapping(address => Student) students;
+    address owner;
 
-  constructor() public{
-  
-    
-}
+    //contract owner
+    constructor() public {
+        owner = msg.sender;
+    }
 
-
-  // function set(uint x) public {
-  //   storedData = x;
-  // }
-
-  // function get() public view returns (uint) {
-  //   return storedData;
-  // }
+    function buyCourse(uint256 courseID) public payable {
+        require(msg.value >= (allCourses[courseID].price));
+        allCourses[courseID].ownerAddress.transfer(allCourses[courseID].price);
+        students[msg.sender].courses.push(courseID);
+    }
 }
